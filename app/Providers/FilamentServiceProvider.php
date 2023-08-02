73123bbb1544f9a923fc8003e14f6e3f494d7512
Arif Hossain <arif04cuet@gmail.com>
2023-08-02 15:44:48 +0600

    wip

--- app/Providers/FilamentServiceProvider.php
+++ app/Providers/FilamentServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Filament\Facades\Filament;
 use Filament\Navigation\NavigationGroup;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\HtmlString;
 use Illuminate\Support\ServiceProvider;
 
 class FilamentServiceProvider extends ServiceProvider
@@ -23,6 +24,15 @@ class FilamentServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Filament::serving(function () {
+
+            Filament::pushMeta([
+                new HtmlString('<link rel="manifest" href="/manifest.json" />'),
+            ]);
+
+            Filament::registerScripts([
+                asset('js/index.js'),
+            ]);
+
             Filament::registerNavigationGroups([
                 'Reseller',
                 'Wholesaler',
