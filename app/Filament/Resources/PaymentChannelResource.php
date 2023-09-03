ca612014bbfa6197cd809788484e87410304405f
Arif Hossain <arif04cuet@gmail.com>
2023-09-03 11:14:57 +0600

    test deploy

--- app/Filament/Resources/PaymentChannelResource.php
+++ app/Filament/Resources/PaymentChannelResource.php
@@ -19,7 +19,7 @@ class PaymentChannelResource extends Resource
 {
     protected static ?string $model = PaymentChannel::class;
 
-    protected static ?string $modelLabel = 'Account';
+    protected static ?string $modelLabel = 'Accounts';
     protected static ?string $navigationIcon = 'heroicon-o-collection';
 
     public static function getEloquentQuery(): Builder
