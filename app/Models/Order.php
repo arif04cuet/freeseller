4f12e190e82ceafb02104e6f83d9027d4ab1fdf7
Arif Hossain <arif04cuet@gmail.com>
2023-10-22 12:18:58 +0600

    steadfast fake

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -308,7 +308,7 @@ class Order extends Model
             $this->forceFill(['status' => OrderStatus::ProcessingForHandOverToCourier->value])->save();
             // add order to steadfast as a parcel
             $order = $this->refresh();
-            if (config('app.env') == 'production')
+            if (config('app.env') == 'production' && 0)
                 AddParcelToSteadFast::dispatch($order);
             else
                 AddParcelToSteadFastFake::dispatch($order);
