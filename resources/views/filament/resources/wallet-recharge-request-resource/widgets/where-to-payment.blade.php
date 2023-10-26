2323be43a1ae55667d5755bbd091dd41af52ef09
Arif Hossain <arif04cuet@gmail.com>
2023-10-26 15:56:23 +0600

    payment info added

--- resources/views/filament/resources/wallet-recharge-request-resource/widgets/where-to-payment.blade.php
+++ resources/views/filament/resources/wallet-recharge-request-resource/widgets/where-to-payment.blade.php
@@ -7,12 +7,18 @@
         <div class="mt-2 flex justify-between flex-col md:flex-row gap-4">
             <div>
                 <strong> Bank Information</strong>
-                <p>Comming Soon..</p>
+                <p><strong>Bank Name</strong> : Bank Asia Limited</p>
+                <p><strong>Branch</strong> : Gulshan Branch</p>
+                <p><strong>A/C Name</strong> : Arif Hossain</p>
+                <p>
+                    <strong>A/C No.</strong> : 00434030848
+                </p>
+
             </div>
 
             <div>
-                <strong> bKash / Nogod </strong>
-                <p>Coming soon..</p>
+                <strong> bKash</strong>
+                <p>01717348147</p>
                 {{-- <p>{{ config('freeseller.platform_bkash') }}</p> --}}
             </div>
         </div>
