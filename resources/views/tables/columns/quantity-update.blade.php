66a137637569e50c53368a99e9e7ebb37e3139dd
Arif Hossain <arif04cuet@gmail.com>
2023-11-12 11:50:46 +0600

    sku qnt update 0

--- resources/views/tables/columns/quantity-update.blade.php
+++ resources/views/tables/columns/quantity-update.blade.php
@@ -4,7 +4,7 @@
 
 @endphp
 <div>
-    <input min="1" oninput="validity.valid||(value='');" placeholder="update new qnt" type="number"
+    <input min="0" oninput="validity.valid||(value='');" placeholder="update new qnt" type="number"
         wire:model.lazy="{{ $name . '.qnt' }}" />
     @if (isset($this->updatedSkus[$getRecord()->id]['status']))
         saved
