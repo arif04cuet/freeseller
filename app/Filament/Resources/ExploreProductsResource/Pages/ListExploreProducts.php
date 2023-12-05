df05b0076800c7d55949603b3b91f4d2c902633e
Arif Hossain <arif04cuet@gmail.com>
2023-12-05 21:37:39 +0600

    return items column fixed

--- app/Filament/Resources/ExploreProductsResource/Pages/ListExploreProducts.php
+++ app/Filament/Resources/ExploreProductsResource/Pages/ListExploreProducts.php
@@ -3,11 +3,13 @@
 namespace App\Filament\Resources\ExploreProductsResource\Pages;
 
 use App\Filament\Resources\ExploreProductsResource;
+use App\Traits\UseSimplePagination;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListExploreProducts extends ListRecords
 {
+    use UseSimplePagination;
     protected static string $resource = ExploreProductsResource::class;
 
     protected function getHeaderActions(): array
